5b1e58acb012382bc0e716fd34e4e1354a2c3122
AKash-47 <z.achilles555@gmail.com>
2024-04-15 19:41:47 +0530

    File is added

---
+++ Task-1.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first value: "))
+k=int(input("Enter second value: "))
+z=int(input("Enter third value: "))
+print(1)
+for i in range(1,z+1):
+    if i%k==0 and i%a==0:
+        print(i)
+
